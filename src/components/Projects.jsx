2875ca200cc1d9424abe5ae73a4db205b009b85f
its-imene <justimene04@gmail.com>
2025-12-09 20:00:14 +0100

    Initial commit with portfolio and images using LFS

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ export default function Projects() {
       
       <div className="projects-list">
        <div className="project"><a href="https://www.figma.com/design/gKq5uUELe9fA5mTW5y6c87/Untitled?node-id=0-1&t=Ap7NAsANBbrOi4A2-1"> <img src={univerdel}/></a></div>
-       <div className="project"><a href="https://www.figma.com/design/baJmPE30ibswAdXZr7nWN6/Untitled?node-id=0-1&t=KEO3jBZohBEeuHZb-1"></a> <img src={game}/></div>
+       <div className="project"><a href="https://www.figma.com/design/baJmPE30ibswAdXZr7nWN6/Untitled?node-id=0-1&t=KEO3jBZohBEeuHZb-1"> <img src={game}/></a> </div>
        {/* <div className="project"></div> */}
         {/* <div className="project"></div> */}
         <button className="cta1"> view more</button>
